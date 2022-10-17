3fdf083574798b7dd54d1d38696f91e14a980262
David Z. Han <zhaohui.han@gmail.com>
2022-10-16 20:15:11 -0400

    Refactor

---
+++ book/clean.sh
@@ -0,0 +1,13 @@
+cd /c/git/q4qbies/book
+rm -rf *.bak
+rm -rf *.aux
+rm -rf *.log
+rm -rf *.toc
+rm -rf *.out
+rm -rf *.synctex.gz
+rm -rf */*.bak
+rm -rf */*.aux
+rm -rf */*.log
+rm -rf */*.toc
+rm -rf */*.out
+rm -rf */*.synctex.gz
